e3d50b4a693a7839381654ca0dded3eb95ef475d
muntahaabdulnabi <muntahaabdulnabi@hotmail.com>
2022-10-22 12:48:18 +0200

    Försöker ta bort en kontakt efter visade detaljer men funker inte

--- AdressBok/MainWindow.xaml.cs
+++ AdressBok/MainWindow.xaml.cs
@@ -27,31 +27,57 @@ namespace AdressBok
         {
             InitializeComponent();
             contacts = new ObservableCollection<ContactPerson>(); //skapar en ny Lista
-            lv_contacts.ItemsSource = contacts;
+            lv_Contacts.ItemsSource = contacts;
         }
-
         private void tbn_Add_Click(object sender, RoutedEventArgs e) 
         {
-            contacts.Add(new ContactPerson
+          var contact = contacts.FirstOrDefault(x => x.Email == tb_Email.Text);
+            if (contact == null)
             {
-                FirstName = tb_FirstName.Text,
-                LastName = tb_LastName.Text,
-                Email = tb_Email.Text,
-                StreetName = tb_StreetName.Text,
-                PostalCode = tb_PostalCode.Text,
-                City = tb_City.Text,
-                
-            }); 
+                contacts.Add(new ContactPerson
+                {
+                    FirstName = tb_FirstName.Text,
+                    LastName = tb_LastName.Text,
+                    Email = tb_Email.Text,
+                    StreetName = tb_StreetName.Text,
+                    PostalCode = tb_PostalCode.Text,
+                    City = tb_City.Text,
+
+                });
+            }
+            else
+            {
+                MessageBox.Show("Email redan finns");
+            }
+            ClearFields();
+            
+        }
+        private void ClearFields()
+        {
+            tb_FirstName.Text = "";
+            tb_LastName.Text = "";
+            tb_Email.Text = "";
+            tb_StreetName.Text = "";
+            tb_PostalCode.Text = "";
+            tb_City.Text = "";
         }
 
         private void tbn_Delete_Click(object sender, RoutedEventArgs e)
         {
-
+            var button = sender as Button;
+            var contact = (ContactPerson)button!.DataContext;
+            contacts.Remove(contact);
         }
 
         private void lv_contacts_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
+        {           
+                var contact = (ContactPerson)lv_Contacts.SelectedItems[0]!;
+                tb_FirstName.Text = contact.FirstName;
+                tb_LastName.Text = contact.LastName;
+                tb_Email.Text = contact.Email;
+                tb_StreetName.Text = contact.StreetName;
+                tb_PostalCode.Text = contact.PostalCode;
+                tb_City.Text = contact.City;            
         }
     }
 }
---
+++ AdressBok/Services/FileManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdressBok.Services
+{
+    internal interface IFileManager
+    {
+        public void Save(string filePath, string content);
+        public void Read(string filePath);
+    }
+    internal class FileManager : IFileManager
+    {
+        public void Read(string filePath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(string filePath, string content)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
